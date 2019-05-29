9d5d3632e3c9023a4b7a4543e1773b2905aadba1
Ching <287758560@qq.com>
2019-05-29 15:04:26 +0800

    fix Rexp/index.js to ES6 module type

--- Rexp/index.js
+++ Rexp/index.js
@@ -1,56 +1,56 @@
-function validatePhone(str) {
+export function validatePhone(str) {
   const reg = /^1[34578]\d{9}$/
   return reg.test(str)
 }
 
-function validateEmail(email) {
+export function validateEmail(email) {
   const reg = /^[a-zA-Z0-9_-]+@([a-zA-Z0-9]+\.)+(com|cn|net|org)$/
   return reg.test(email)
 }
 
-function validatePositiveInteger(num) {
+export function validatePositiveInteger(num) {
   const reg = /^[1-9]\d*$/
   return reg.test(num)
 }
 
-function validateIdNumber(idNum) {
+export function validateIdNumber(idNum) {
   const reg = /^(\d{6})(\d{4})(\d{2})(\d{2})(\d{3})([0-9]|X)$/
   return reg.test(idNum)
 }
 
-function getUrlQueryParams() {
+export function getUrlQueryParams() {
   let paramsObj = {}
   location.search.replace(/([^?&=]+)=([^&]+)/g, (_, k, v) => paramsObj[k] = v)
   return paramsObj
 }
 
-function lowercaseLetter(str) {
+export function lowercaseLetter(str) {
   const reg = /^[a-z]+$/
   return reg.test(str)
 }
 
-function uppercaseLetter(str) {
+export function uppercaseLetter(str) {
   const reg = /^[A-Z]+$/
   return reg.test(str)
 }
 
-function letter(str) {
+export function letter(str) {
   const reg = /^[A-Z]+$/
   return reg.test(str)
 }
 
-function isIncludesWhiteSpace(str) {
+export function isIncludesWhiteSpace(str) {
   if (str) return str.includes(' ')
 }
 
-function isIncludesDot(str) {
+export function isIncludesDot(str) {
   if (str) return str.includes('.')
 }
 
-function isLongerThan(str, length) {
+export function isLongerThan(str, length) {
   if (str) return str.length > length
 }
 
-function isShorterThan(str, length) {
+export function isShorterThan(str, length) {
   if (str) return str.length < length
 }
