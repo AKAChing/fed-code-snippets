3a5eba7a36860bffeadb443af538a93856ae3daf
akaching <akaching@foxmail.com>
2020-03-09 14:59:29 +0800

    add get random num function

---
+++ GetRandomNum/index.js
@@ -0,0 +1,14 @@
+// 获取指定范围内的随机数
+export function randomNum(minNum, maxNum) {
+  switch (arguments.length) {
+    case 1:
+      return parseInt(Math.random() * minNum + 1, 10);
+      break;
+    case 2:
+      return parseInt(Math.random() * (maxNum - minNum + 1) + minNum, 10);
+      break;
+    default:
+      return 0;
+      break;
+  }
+}
